580e06f9a04dd0617b23e8d6e1edd0c78d2b8d8d
Nabiz <dawcio.gwizdz@gmail.com>
2023-04-19 15:13:36 +0200

    car demo scene

--- VRCarSimulator/Assets/Prefabs/Car 01/Scripts/CarControllerWheel.cs
+++ VRCarSimulator/Assets/Prefabs/Car 01/Scripts/CarControllerWheel.cs
@@ -10,6 +10,7 @@ public class CarControllerWheel : MonoBehaviour
     private bool isBreaking;
 
     private bool isStarted = false;
+    private bool gearForrward = true;
 
     // Settings
     [SerializeField] private float motorForce, breakForce, maxSteerAngle;
@@ -22,20 +23,32 @@ public class CarControllerWheel : MonoBehaviour
     [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
     [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
 
-    private void FixedUpdate()
+    [SerializeField] private Transform steerWheel;
+    double speed = 0f;
+
+    private void Update()
     {
-        if (isStarted)
-        {
-            GetInput();
-            Debug.Log(3.6 * frontLeftWheelCollider.rpm * 2 * Math.PI * frontLeftWheelCollider.radius/60);
-            HandleMotor();
-            HandleSteering();
-            UpdateWheels();
-        }
         if (Input.GetKey(KeyCode.Space))
         {
             isStarted = true;
         }
+
+        if (isStarted)
+        {
+            GetInput();
+        }
+    }
+    private void FixedUpdate()
+    {
+        speed = 3.6 * gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+        if (isStarted)
+        {
+            //Debug.Log(speed);
+            HandleMotor();
+            HandleSteering();
+            UpdateWheels();
+        }
+        steerWheel.localEulerAngles = new Vector3(0f, 0f, -horizontalInput * 90f);
     }
 
     private void GetInput()
@@ -47,13 +60,33 @@ public class CarControllerWheel : MonoBehaviour
         verticalInput = (Input.GetAxis("VerticalGas") + 1.0f)/2.0f;
 
         // Breaking Input
-        breakInput = (Input.GetAxis("VerticalBreak") + 1.0f) / 2.0f; ;
+        breakInput = (Input.GetAxis("VerticalBreak") + 1.0f) / 2.0f;
+        
+        if (Input.GetButtonDown("LowerGear") && speed < 4f)
+        {
+            Debug.Log("DUPA");
+            gearForrward = false;
+        }
+        if (Input.GetButtonDown("HigherGear") && speed < 4f)
+        {
+            Debug.Log("WIEKSZA DUPA");
+            gearForrward = true;
+        }
     }
 
     private void HandleMotor()
     {
-        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
-        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        if (gearForrward)
+        {
+            frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+            frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        }
+        else
+        {
+            frontLeftWheelCollider.motorTorque = -verticalInput * motorForce;
+            frontRightWheelCollider.motorTorque = -verticalInput * motorForce;
+        }
+
         //currentbreakForce = isBreaking ? breakForce : 0f;
         ApplyBreaking();
     }
